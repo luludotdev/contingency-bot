e4375d02aff7b185e3b1b83503e2041a99fd9bcc
Jack Baron <jackmbaron@gmail.com>
2021-07-23 14:10:48 +0100

    change embed colour when cancelled

--- src/interactions/vote/cancel.ts
+++ src/interactions/vote/cancel.ts
@@ -5,6 +5,7 @@ export const vote__cancel: Handler = async ({ manager, button }) => {
   if (!manager.voteInProgress()) {
     const embed = button.message.embeds[0]
     embed.setDescription(`~~${embed.description}~~\n**This vote has expired.**`)
+    embed.setColor(0x4f_54_5c)
 
     await cancelVote(button, embed)
   }
