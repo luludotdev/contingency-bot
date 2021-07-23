1262b00f133beb45b303c2ef9ce69d5dc80399a4
Jack Baron <jackmbaron@gmail.com>
2021-07-23 16:46:38 +0100

    implement castVote()

--- src/manager.ts
+++ src/manager.ts
@@ -102,8 +102,13 @@ export const createManager: () => Manager = () => {
     },
 
     castVote(member) {
-      // TODO
-      throw new Error('Not Implemented')
+      if (vote === null) throw new Error('no vote in progress')
+      if (!this.canVote(member)) throw new Error('not allowed')
+
+      const weight = this.voteWeight(member)
+      vote.votes.set(member.id, [member, weight])
+
+      return vote
     },
 
     hasVoted(member) {
