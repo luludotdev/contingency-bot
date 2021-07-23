544b13bad1466fec59ab50c87a717ae599a3820d
Jack Baron <jackmbaron@gmail.com>
2021-07-23 16:45:45 +0100

    approve button logic

--- src/interactions/vote/approve.ts
+++ src/interactions/vote/approve.ts
@@ -12,16 +12,37 @@ export const vote__approve: Handler = async ({ manager, button }) => {
     return
   }
 
-  if (manager.isTarget(button.clicker.member)) {
+  const member = button.clicker.member
+  const reply = async (message: string) => {
     await button.reply.send(
-      'You may not approve as you are the target of this vote.',
+      message,
       // @ts-expect-error
       true
     )
+  }
 
+  if (manager.isTarget(member)) {
+    await reply('You may not approve as you are the target of this vote.')
     return
   }
 
-  // TODO
-  console.log('vote@approve')
+  if (!manager.canVote(member)) {
+    await reply('You are not allowed to do that.')
+    return
+  }
+
+  if (manager.hasVoted(member)) {
+    await reply('You have already voted!')
+    return
+  }
+
+  await button.reply.defer(true)
+  const vote = manager.castVote(button.clicker.member)
+
+  const embed = button.message.embeds[0]
+  embed.fields[0].value = vote.voters
+
+  await button.message.edit({ embed })
+
+  // TODO: Check when a vote threshold has been met
 }
