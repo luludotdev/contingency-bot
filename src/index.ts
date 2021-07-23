b61dd854bf81d0c0e6ac1e051435f5f22545720d
Jack Baron <jackmbaron@gmail.com>
2021-07-23 08:04:45 +0100

    flush logs on exit

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,18 @@
+import { field } from '@lolpants/jogger'
 import sourceMapSupport from 'source-map-support'
+import { exitHook } from './exit.js'
+import { errorField, flush, logger } from './logger.js'
 
 // Enable Source Maps
 sourceMapSupport.install()
+
+exitHook(async (exit, error) => {
+  if (error) {
+    logger.error(errorField(error))
+  } else {
+    logger.info(field('action', 'shutdown'))
+  }
+
+  await flush()
+  exit()
+})
