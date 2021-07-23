dbeed109821897c31a28f5a0a6b7b5fadf60a6a4
Jack Baron <jackmbaron@gmail.com>
2021-07-23 14:59:05 +0100

    env for role related things

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,4 +1,4 @@
-import { registerString } from './register.js'
+import { registerInt, registerString } from './register.js'
 
 // #region Globals
 const NODE_ENV = registerString('NODE_ENV')
@@ -9,4 +9,34 @@ export const IS_DEV = !IS_PROD
 // #region Bot
 export const TOKEN = registerString('TOKEN', true)
 export const GUILD_ID = registerString('GUILD_ID', true)
+// #endregion
+
+// #region Roles
+const roleWeights = registerString('ROLE_WEIGHTS', true)
+const split = roleWeights.split(';').map(entry => entry.split(','))
+
+const mapped: Array<[roleID: string, weight: number]> = split.map(
+  ([key, value]) => [key, Number.parseInt(value, 10)]
+)
+
+const roleRX = /^\d+$/
+for (const [roleID, weight] of mapped) {
+  if (!roleRX.test(roleID)) {
+    throw new TypeError(`roleID \`${roleID}\` is invalid!`)
+  }
+
+  if (Number.isNaN(weight)) {
+    throw new TypeError(`weight for \`${roleID}\` is not a number!`)
+  }
+
+  if (weight < 0) {
+    throw new TypeError(`weight for \`${roleID}\` must be greater than 0!`)
+  }
+}
+
+const weightMap = new Map(mapped)
+export { weightMap as ROLE_WEIGHTS }
+
+export const VOTING_WEIGHT = registerInt('VOTING_WEIGHT', true)
+export const TARGET_SCORE = registerInt('TARGET_SCORE', true)
 // #endregion
