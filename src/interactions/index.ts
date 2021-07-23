792c3e3ece266d34179885245176e9aadbf311f8
Jack Baron <jackmbaron@gmail.com>
2021-07-23 12:30:11 +0100

    add interaction ID helpers

---
+++ src/interactions/index.ts
@@ -0,0 +1,25 @@
+export const interactionID: (
+  context: string,
+  id: string,
+  ...components: string[]
+) => string = (context, id, ...components) => {
+  const start = `${context}@${id}`
+  if (components.length === 0) return start
+
+  const joined = components.join('/')
+  return `${start}/${joined}`
+}
+
+interface ParsedInteraction {
+  context: string
+  id: string
+  key: string
+  components: string[]
+}
+
+export const parseInteractionID: (id: string) => ParsedInteraction = rawID => {
+  const [key, ...components] = rawID.split('/')
+  const [context, id] = key.split('@')
+
+  return { context, id, key, components }
+}
